b3b35f23ff31b1fb1b7c1fb06144375265af991a
cfop2357 <yoguel.salazar@gmail.com>
2021-04-18 15:55:45 -0500

    left canvas

--- GUI/mammography_visualization.py
+++ GUI/mammography_visualization.py
@@ -41,19 +41,19 @@ def open_mini_image(img):
     height_percent = (fixed_height / float(img.size[1]))
     width_size = int((float(img.size[0]) * float(height_percent)))
     img = img.resize((int(width_size), int(fixed_height)))
-  if img.size[1] > 300: #adjusr width
+  if img.size[1] > 300: #adjust width
     fixed_width = float(300)
     width_percent = (fixed_width / float(img.size[0]))
     height_size = int((float(img.size[1]) * float(width_percent)))
     img = img.resize((int(fixed_width), int(height_size)))
   # Show image
-  img = ImageTk.PhotoImage(img)
-  miniImg.configure(image = img)
-  miniImg.image = img
+  global leftImg
+  leftImg = ImageTk.PhotoImage(img)
+  leftSide.create_image(0, 0, anchor=NW, image=leftImg)
 
-def change_zoom_image(img):
+def change_zoom_image(img, to_select):
   """open and cut img of the right"""
-  img = img.crop([0,0,1200,700])
+  img = img.crop(to_select)
   # Show image
   img = ImageTk.PhotoImage(img)
   zoomImg.configure(image = img)
@@ -63,7 +63,7 @@ def open_image(fpath):
   """open an image"""
   global originalImg
   originalImg = dcm_to_PIL_image_gray(fpath)
-  change_zoom_image(originalImg)
+  change_zoom_image(originalImg, [0,0,1200,700])
   open_mini_image(originalImg)
 
 
@@ -73,8 +73,7 @@ def click_on_open():
   global originalImg
   # Open image
   root.filename = filedialog.askopenfilename(title="Selecciona un archivo", filetypes=(("DICOM", "*.dcm"), ("", "")))
-  originalImg = dcm_to_PIL_image_gray(root.filename)
-  openMiniImg(root.filename)
+  open_image(root.filename)
 
 def fullScreen():
   """screen in full size"""
@@ -95,12 +94,15 @@ if __name__ == "__main__":
   openButton = Button(root, text = "Abrir", command=click_on_open)
   openButton.grid(row = 0, column = 0)
   # Init img
-  originalImg = ImageTk.PhotoImage(Image.open("emptyIMG.jpg"))
-  miniImg = Label(root, image = originalImg)
-  miniImg.grid(row = 1, column = 0)
-  zoomImg = Label(root, image = originalImg)
+  originalImg = Image.open("emptyIMG.jpg")
+  leftSide = Canvas(root, width = 305, height = 505)
+  leftSide.grid(row = 1, column = 0)
+  leftImg = ImageTk.PhotoImage(originalImg)
+  leftSide.create_image(0, 0, anchor=NW, image=leftImg)
+  zoomImg = Label(root, image = leftImg)
   zoomImg.grid(row = 1, column = 2)
   open_image('example.dcm')
+
   # Start window
   root.mainloop();
 
