2a2916a8c05436922ac1eb7b6785bc6bfd2e9ce9
cfop2357 <yoguel.salazar@gmail.com>
2021-04-18 14:22:35 -0500

    adjust left img

--- GUI/mammography_visualization.py
+++ GUI/mammography_visualization.py
@@ -1,6 +1,6 @@
-__author__ = "Hernandez Hernandez Bernado and Salazar Alanís Víctor Yoguel"
+__author__ = "Hernandez Hernandez Bernado and Salazar Alanis Victor Yoguel"
 # __copyright__ = ""
-__credits__ = ["Hernandez Hernandez Bernado", "Salazar Alanís Víctor Yoguel"]
+__credits__ = ["Hernandez Hernandez Bernado", "Salazar Alanis Victor Yoguel"]
 # __license__ = "GPL"
 # __version__ = "0.0.1"
 __date__ = "2021-03-15"
@@ -36,39 +36,60 @@ def dcm_to_PIL_image_gray(fpath):
 
   return pixel_array_to_gray(ds.pixel_array)  # Aun se ve borroso
 
+def openMiniImg(fpath):
+  """abre la imagen miniatura en la izquierda"""
+  img = dcm_to_PIL_image_gray(fpath)
+  # Resize image
+  if img.size[0] > 500: #adjust height
+    fixed_height = float(500)
+    height_percent = (fixed_height / float(img.size[1]))
+    width_size = int((float(img.size[0]) * float(height_percent)))
+    img = img.resize((int(width_size), int(fixed_height)))
+  if img.size[1] > 300: #adjusr width
+    fixed_width = float(300)
+    width_percent = (fixed_width / float(img.size[0]))
+    height_size = int((float(img.size[1]) * float(width_percent)))
+    img = img.resize((int(fixed_width), int(height_size)))
+  # Show image
+  img = ImageTk.PhotoImage(img)
+  currentImage.configure(image = img)
+  currentImage.image = img
+
+
 
 def click_on_open():
   """Select a DICOM file and show it on screen using the PIL packege"""
   # Open image
   root.filename = filedialog.askopenfilename(title="Selecciona un archivo", filetypes=(("DICOM", "*.dcm"), ("", "")))
-  img = dcm_to_PIL_image_gray(root.filename)
+  openMiniImg(root.filename)
 
-  # Resize image
-  fixed_height = float(600)
-  height_percent = (fixed_height / float(img.size[1]))
-  width_size = int((float(img.size[0]) * float(height_percent)))
-  img = img.resize((int(width_size), int(fixed_height)))
   
-  # Show image
-  img = ImageTk.PhotoImage(img)
 
-  currentImage.configure(image = img)
-  currentImage.image = img
+def fullScreen():
+  """screen in full size"""
+  #getting screen width and height of display
+  width= root.winfo_screenwidth() 
+  height= root.winfo_screenheight()
+  #setting tkinter window size
+  root.geometry("%dx%d" % (width, height))
+  root.title("Geeeks For Geeks")
   
   
 if __name__ == "__main__":
   # Init window
   root = Tk()
   root.title("visualización de mamografias")
+  fullScreen()
 
-  # Add Buttons
+  # Add Buttonsñññ
   openButton = Button(root, text = "Abrir", command=click_on_open)
-  openButton.grid(row = 0, column = 0);
+  openButton.grid(row = 0, column = 0)
 
   # Init img
   img = ImageTk.PhotoImage(Image.open("emptyIMG.jpg"))
   currentImage = Label(root, image = img)
   currentImage.grid(row = 1, column = 0)
+  openMiniImg('example.dcm')
 
   # Start window
   root.mainloop();
