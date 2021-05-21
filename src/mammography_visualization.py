0d50e847c0628fd65286254723814523cde8c40d
cfop2357 <yoguel.salazar@gmail.com>
2021-05-21 17:52:15 -0500

    bugg

--- src/mammography_visualization.py
+++ src/mammography_visualization.py
@@ -81,5 +81,7 @@ if __name__ == "__main__":
     filter_button_text.set("Filtrar")
     image_displayer = ImageDisplayer(root, width=1200, height=750)
     selection_image = SelectionImage(root, width=305, height=505, displayer=image_displayer)
+    #oppening only once at the begining put the rectangle on a "random position" i dont know why, is a bugg
+    open_image('example.dcm')
     open_image('example.dcm')
     root.mainloop()
