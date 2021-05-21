09e0e6fcfc7be54d5ccbde418d9381f674c01383
bhdzhdz <Bhdzhdz@gmail.com>
2021-05-21 13:54:57 -0500

    Add type hints

--- GUI/ImageDisplayer.py
+++ GUI/ImageDisplayer.py
@@ -1,13 +1,12 @@
 import tkinter
 from tkinter import *
-from tkinter import filedialog
-from PIL import ImageTk, Image
-import typing
+from PIL import ImageTk
+from type_definitions import *
 
 
 class ImageDisplayer:
 	"""Display a segment of an image"""
-	def __init__(self, root: tkinter.Tk, width: int, height: int, row: int = 1, column: int = 2):
+	def __init__(self, root: tkinter.Tk, width: int, height: int, row: int = 1, column: int = 2) -> None:
 		self.width = width
 		self.height = height
 		self.label = Label(root)
@@ -16,20 +15,31 @@ class ImageDisplayer:
 		self.current_segment = None
 
 	@property
-	def image(self):
+	def image(self) -> PilImage:
 		return self.current_image
 
 	@image.setter
-	def image(self, img):
+	def image(self, img: PilImage) -> None:
 		"""Update image"""
 		self.current_image = img
 
-	def change_segment(self, to_select):
-		"""change the segment of image wich is going to be show"""
+	@property
+	def segment(self) -> TkImage:
+		return self.current_segment
+
+	@segment.setter
+	def segment(self, to_select: Coord) -> None:
+		"""change the segment of image which is going to be show"""
 		self.current_segment = self.image.crop(to_select)
 		self.current_segment = ImageTk.PhotoImage(self.current_segment)
 		self.show()
 
-	def show(self):
+	def change_segment(self, to_select: Coord) -> None:
+		"""change the segment of image which is going to be show"""
+		self.current_segment = self.image.crop(to_select)
+		self.current_segment = ImageTk.PhotoImage(self.current_segment)
+		self.show()
+
+	def show(self) -> None:
 		"""show the segment image"""
 		self.label.configure(image=self.current_segment)
